530a6d77071adb47bfbb1629732b4468ea4074e7
jugalpanchal007 <123182182+jugalpanchal007@users.noreply.github.com>
2023-02-17 15:06:11 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/dto/OrderRequest.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+
+	private Customer customer;
+}
---
+++ src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.management.relation.Role;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String email;
+	
+	@OneToMany(targetEntity = Product.class,cascade = CascadeType.ALL)
+	@JoinColumn(name = "cp_fk",referencedColumnName = "id")
+	private List<Product> products;
+	
+}
